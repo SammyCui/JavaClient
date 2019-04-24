53c838ba672c17a76cd8ef17d2ff67152d44fe72
wazzzaonthebeat <42841110+wazzzaonthebeat@users.noreply.github.com>
2019-04-24 11:40:51 -0400

    hello

---
+++ src/Client.java
@@ -0,0 +1,63 @@
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.EventListener;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Socket sock;
+        BufferedReader from;
+        PrintWriter to;
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter IP address: ");
+        String ip = kbd.nextLine().trim();
+
+        System.out.println("hello");
+
+        try {
+            sock = new Socket(ip, 12346);
+
+            //client successfully connect to server
+            System.out.println("Connected to " +
+                    sock.getInetAddress()); //hello
+
+
+            System.out.print("Press Enter to request a quote: ");
+            String enter = kbd.nextLine().trim();
+
+
+
+            from = new BufferedReader(
+                    new InputStreamReader(
+                            sock.getInputStream()
+                    )
+            );
+            to = new PrintWriter(sock.getOutputStream(),
+                    true);
+            while (true) {
+                System.out.println("Waiting ...");
+                String response = from.readLine();
+                System.out.println("them: " + response);
+                System.out.print("me: ");
+                String s = kbd.nextLine();
+                to.println(s);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+}
---
+++ src/Server.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        ServerSocket sock;
+        Socket client;
+        BufferedReader from;
+        PrintWriter to;
+
+        Scanner kbd = new Scanner(System.in);
+
+        try {
+            sock = new ServerSocket(12346);
+            System.out.println("Waiting for connection ...");
+            client = sock.accept();
+            System.out.println("Connected to " +
+                    client.getInetAddress());
+            from = new BufferedReader(
+                    new InputStreamReader(
+                            client.getInputStream()
+                    )
+            );
+
+            to = new PrintWriter(client.getOutputStream(),
+                    true);
+
+            while (true) {
+                System.out.println("me: ");
+                String s = kbd.nextLine();
+                to.println(s);
+                String response = from.readLine();
+                System.out.println(response);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
